4a392f1b6eee98984813511a9d70de1a4d56a57f
Kenneth V. Domingo <hello@kvd.studio>
2025-02-09 14:04:42 +0800

    feat: remove default layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import Link from "next/link";
+import { DarkModeToggle } from "./DarkModeToggle";
+
+export function Navbar() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="/" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Home
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <DarkModeToggle />
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
